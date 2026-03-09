32a003ac607966526457c3b82a00d5ef2befffb3
MARÍA ABASCAL <mariaabascal06@gmail.com>
2026-03-09 13:42:06 +0100

    Vuetify instalado manualmente y funcionando con estilos

--- vue-introduccion/src/App.vue
+++ vue-introduccion/src/App.vue
@@ -1,42 +1,32 @@
 <template>
   <v-app>
-    
     <v-app-bar color="primary" density="compact">
       <v-app-bar-title>Mi Primer Proyecto Vuetify</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
-      
       <v-container>
-        
         <v-card class="mx-auto mt-5" max-width="400" elevation="4">
-          
           <v-card-item>
             <v-card-title>¡Hola, Mundo!</v-card-title>
             <v-card-subtitle>Probando componentes</v-card-subtitle>
           </v-card-item>
 
           <v-card-text>
-            Este es mi primer componente de prueba usando la documentación de Vuetify. Poco a poco iré mirando y añadiendo más cosas q vaya haciendo nuevas.
+            Ahora sí que tengo Vuetify bien instalado. ¡A probar componentes!
           </v-card-text>
 
           <v-card-actions>
-            <v-spacer></v-spacer> <v-btn color="success" variant="flat">
+            <v-spacer></v-spacer>
+            <v-btn color="success" variant="flat">
               Púlsame
             </v-btn>
           </v-card-actions>
-          
         </v-card>
-
       </v-container>
     </v-main>
-    
   </v-app>
 </template>
 
 <script setup>
 </script>
-
-<style>
-/*no hace falta poner css pq ya se encarga el Vuetify*/
-</style>
--- vue-introduccion/src/main.js
+++ vue-introduccion/src/main.js
@@ -1,6 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Importaciones de Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// Configuración de Vuetify
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// Creación de la app usando Vuetify
+createApp(App).use(vuetify).mount('#app')
