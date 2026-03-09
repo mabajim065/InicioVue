171d743d0b9cca09f03fd76e7b0921a4fbbfc936
MARÍA ABASCAL <mariaabascal06@gmail.com>
2026-03-09 14:06:12 +0100

    Creada carpeta views y componente HomeView

--- vue-introduccion/src/App.vue
+++ vue-introduccion/src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar color="primary" density="compact">
-      <v-app-bar-title>Mi Primer Proyecto Vuetify</v-app-bar-title>
+    <v-app-bar color="teal-darken-3" elevation="2">
+      <v-app-bar-title>Mi Proyecto Paso a Paso</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
-      <v-container>
-        <v-card class="mx-auto mt-5" max-width="400" elevation="4">
-          <v-card-item>
-            <v-card-title>¡Hola, Mundo!</v-card-title>
-            <v-card-subtitle>Probando componentes</v-card-subtitle>
-          </v-card-item>
-
-          <v-card-text>
-            Ahora sí que tengo Vuetify bien instalado. ¡A probar componentes!
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="success" variant="flat">
-              Púlsame
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-container>
+      <HomeView />
     </v-main>
+
   </v-app>
 </template>
 
 <script setup>
+// importo el archivo creado en la carpeta view
+import HomeView from './views/HomeView.vue'
 </script>
---
+++ vue-introduccion/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-container class="text-center mt-10">
+    <h1 class="text-teal-darken-3">Página de Inicio</h1>
+    <p>Poco a poco voya ir  montando la estructura.</p>
+  </v-container>
+</template>
+
+<script setup>
+// Esta es nuestra primera vista independiente
+</script>
