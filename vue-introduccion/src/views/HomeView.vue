234d7230edb8261d16ad7dd391e87618f2e794f1
Maria Abascal <mariaabascal06@gmail.com>
2026-03-10 10:19:53 +0100

    uso grid para ver multiples tarjetas

--- vue-introduccion/src/views/HomeView.vue
+++ vue-introduccion/src/views/HomeView.vue
@@ -1,13 +1,31 @@
 <template>
-  <v-container class="text-center mt-10">
-    <h1 class="text-pink-darken-3 mb-5">Página de Inicio</h1>
-    <p class="mb-8">Aquí debajo voy a poner mi nuevo componente:</p>
+  <v-container class="mt-10">
+    
+    <div class="text-center mb-10">
+      <h1 class="text-pink-darken-3 mb-2">Nuestro Equipo</h1>
+      <p class="text-medium-emphasis">Conoce a los usuarios de nuestra empresa.</p>
+    </div>
 
-    <UserCard />
+    <v-row>
+      
+      <v-col cols="12" sm="6" md="4">
+        <UserCard />
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <UserCard />
+      </v-col>
+
+      <v-col cols="12" sm="6" md="4">
+        <UserCard />
+      </v-col>
+
+    </v-row>
     
   </v-container>
 </template>
 
 <script setup>
+// Seguimos importando nuestra pieza de Lego exacta de antes
 import UserCard from '../components/UserCard.vue'
 </script>
