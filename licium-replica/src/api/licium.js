81842e605f46118c82cd31af3f82584c25658a54
Maria Abascal <mariaabascal06@gmail.com>
2026-03-19 10:17:56 +0100

    api

--- licium-replica/src/api/licium.js
+++ licium-replica/src/api/licium.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'https://arcadium.cluster24.libnamic.eu/api/glam'
+  baseURL: '/api/glam'
 })
 
 // Obtiene el listado de records con paginación
