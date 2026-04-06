c8c53910cd94eb201ad540282ba95e3d3f88f93b
Maria Abascal <mariaabascal06@gmail.com>
2026-04-06 11:45:24 +0200

    cambios de record a singular

--- licium-replica/src/api/licium.js
+++ licium-replica/src/api/licium.js
@@ -22,11 +22,11 @@ export function getRecords(offset = 0, limit = 24, collectionId = null) {
       children: [{ type: 'condition', field: 'collections', operator: 'in', value: [Number(collectionId)] }]
     })
   }
-  return api.get('/records', { params })
+  return api.get('/record', { params }) // <-- CORREGIDO a singular
 }
 
 export function getRecordDetail(id) {
-  return api.get(`/records/${id}`, {
+  return api.get(`/record/${id}`, { // <-- CORREGIDO a singular
     params: {
       with_labels: 1,
       fields: 'id,title,description,canonical_joined_metadata,joined_metadata,thumbnail,collections.id,collections.title,media_items.id,media_items.path,media_items.thumbnail,media_items.title'
@@ -74,5 +74,5 @@ export function searchRecords({ query = '', collectionId = '', offset = 0, limit
     params['domain'] = JSON.stringify({ op: 'and', children })
   }
 
-  return api.get('/records', { params })
+  return api.get('/record', { params })
 }
